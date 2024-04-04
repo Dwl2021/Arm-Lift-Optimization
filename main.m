520a595520aeec3b2361cc1c895839dc21c24188
Deng Weiliang <Dengwl.2021@gmail.com>
2024-04-04 13:06:20 +0800

    [Feat][other]: add path search method —— JPS

--- main.m
+++ main.m
@@ -1,19 +1,79 @@
 clear;close all;clc;
-figure('Position', [200, 200, 800, 400]);
-axis([0 120 0 60]);
-plot_quadrotor(8,8, pi/6,5);
-plot_object();
+
+mapsize = [200,100] % x,y
+map = ones(mapsize(1), mapsize(2));
+figure('Position', [200, 200, 800, 800 * mapsize(2) / mapsize(1)]);
+axis([0 mapsize(1) 0 mapsize(2)]);
+hold on;
+map = add_obstacle(map);
+map_dilation = mapDilation(map, 7);
+map_dilation = add_boundary(map_dilation);
+
+start = [10, 10];
+goal = [150, 48];
+
+[path, goal_reached, cost, EXPAND] = jps(map_dilation, start, goal);
+
+% plot map
+x_obstacle = [];
+y_obstacle = [];
+for i = 1:mapsize(1)
+    for j = 1:mapsize(2)
+        if map(i,j) == 2
+            x_obstacle = [x_obstacle, i];
+            y_obstacle = [y_obstacle, j];
+        end
+    end
+end
+scatter(x_obstacle, y_obstacle, 10, 'filled', 'MarkerFaceColor', 'black');
+hold on;
+
+if goal_reached
+    plot(path(:,1), path(:,2), 'Color', 'red', 'LineWidth', 2);
+end
+
+
+plot_quadrotor(start(1),start(2)+2, pi/6,5);
 
 
 
-function [] = plot_object()
-    x = 80;
-    y = 0;
-    width = 10;
-    height = 25;
-    hold on; % Hold the plot to draw all parts
-    color = 'red';
-    fill([x, x + width, x + width, x], [y, y, y + height, y + height], color); 
+function [map] = add_obstacle(map)
+    x = size(map,1) *0.7;
+    y = 1;
+    width = size(map,1) * 0.1;
+    height = size(map,2) * 0.4;
+    for i = x:x+width
+        for j = y:y+height
+            map(i,j) = 2;
+        end
+    end
+
+    x = size(map,1) *0.5;
+    y = 1;
+    width = size(map,1) * 0.1;
+    height = size(map,2) * 0.7;
+    for i = x:x+width
+        for j = y:y+height
+            map(i,j) = 2;
+        end
+    end
+
+    x = size(map,1) *0.5;
+    y = size(map,2);
+    width = size(map,1) * 0.1;
+    height = size(map,2) * 0.1;
+    for i = x:x+width
+        for j = y-height:y
+            map(i,j) = 2;
+        end
+    end
+end
+
+function [map] = add_boundary(map)
+    map(1,:) = 2;
+    map(end,:) = 2;
+    map(:,1) = 2;
+    map(:,end) = 2;
 end
 
 
@@ -43,4 +103,12 @@ function [] = plot_quadrotor(x, y, theta, armlength)
     plot(arm4(2,1), arm4(2,2), 'o', 'Color', '#7FFF00', 'MarkerSize', 10, 'MarkerFaceColor', '#7FFF00');
     plot(prop1(:,1), prop1(:,2), 'Color', color, 'LineWidth', lineWidth);
     plot(prop2(:,1), prop2(:,2), 'Color', color, 'LineWidth', lineWidth);
+end
+
+function dilatedMap = mapDilation(Map, dilationRadius)
+    binaryMap = Map == 2;
+    se = strel('disk', dilationRadius);
+    dilatedMap = imdilate(binaryMap, se);
+    dilatedMap = double(dilatedMap);
+    dilatedMap(dilatedMap == 1) = 2;
 end
